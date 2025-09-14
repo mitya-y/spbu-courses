6d60550db64775528ae525e2fa050eb5996f0f1c
Dmitriy Kozenko <8863314@gmail.com>
2025-09-15 01:46:21 +0300

    improved solver

--- 3-course/eval-methods/task1/main.cpp
+++ 3-course/eval-methods/task1/main.cpp
@@ -1,109 +1,7 @@
-#include <iostream>
-#include <print>
-#include <functional>
-#include <numeric>
+#include "solver/solver.hpp"
 
-#include "muParser.h"
-
-using ComputedFunc = std::function<double(double)>;
-
-struct Range {
-  double start = 0, end = 0;
-
-  static Range parse() {
-    Range range;
-    std::print("input range start: ");
-    std::cin >> range.start;
-    std::print("input range end: ");
-    std::cin >> range.end;
-    return range;
-  }
-
-  Range(double start = 0, double end = 0) : start(start), end(end) {}
-  std::string to_str() const {
-    return std::format("[{}, {}]", start, end);
-  }
-  double length() const { return end - start; }
-  std::tuple<double, double> bounds() const { return {start, end}; }
-};
-
-static double bissection_method(const Range &range, double eps, ComputedFunc input_func) {
-  auto [start, end] = range.bounds();
-  auto func = input_func;
-  if (std::abs(func(start)) < eps) {
-    return start;
-  }
-  if (std::abs(func(end)) < eps) {
-    return end;
-  }
-
-  // We always work with increased function - this change is not make sense for root
-  if (func(end) < 0) {
-    func = [input_func](double x) { return -input_func(x); };
-  }
-
-  while (true) {
-    double mid = std::midpoint(start, end);
-    double value = func(mid);
-    if (std::abs(value) <= eps) {
-      return mid;
-    }
-    if (value > 0) {
-      end = mid;
-    } else {
-      start = mid;
-    }
-  }
-  return std::numeric_limits<double>::max();
-}
-
-static double newton_method(const Range &range, double eps, ComputedFunc func, ComputedFunc df) {
-  double x = std::midpoint(range.start, range.end);
-  while (std::abs(func(x)) >= eps) {
-    x = x - (func(x) / df(x));
-  }
-  return x;
-}
-
-static double updated_newton_method(const Range &range, double eps, ComputedFunc func, ComputedFunc df) {
-  double x = std::midpoint(range.start, range.end);
-  double dfx0 = df(x);
-  while (std::abs(func(x)) >= eps) {
-    x = x - (func(x) / dfx0);
-  }
-  return x;
-}
-
-static double method_secants(const Range &range, double eps, ComputedFunc func) {
-  double x0 = range.start;
-  double x1 = range.end;
-  while (std::abs(func(x1)) >= eps) {
-    auto save = x1;
-    double fx1 = func(x1);
-    double inv_df = (x1 - x0) / (fx1 - func(x0));
-    x1 = x1 - (fx1 * inv_df);
-    x0 = save;
-  }
-  return x1;
-}
-
-std::vector<Range> split_ranges(const Range &range, ComputedFunc f, ComputedFunc df,
-                                uint32_t step_numbers = 40) {
- std::vector<Range> change_sign_ranges;
-  double step = range.length() / step_numbers;
-  double start = range.start;
-  for (uint32_t i = 0; i < step_numbers; i++) {
-    double end = start + step;
-    if (f(start) * f(end) <= 0) {
-      change_sign_ranges.emplace_back(start, end);
-    }
-    start = end;
-  }
-  return change_sign_ranges;
-}
-
-std::vector<float> solve(const Range &range, float eps, ComputedFunc f, ComputedFunc df) {
-  auto change_sign_ranges = split_ranges(range, f, df);
+void interractive_solve(const Range &range, float eps, ComputedFunc f, ComputedFunc df) {
+  auto change_sign_ranges = split_ranges(range, f);
 
   std::println("ranges with sign changing:");
   for (uint32_t i = 0; i < change_sign_ranges.size(); i++) {
@@ -117,22 +15,24 @@ std::vector<float> solve(const Range &range, float eps, ComputedFunc f, Computed
     throw std::runtime_error(std::format("invalid range_idx: {}", range_idx));
   }
 
-  // double root = bissection_method(change_sign_ranges[range_idx], eps, f);
-  // double root = newton_method(change_sign_ranges[range_idx], eps, f, df);
-  // double root = updated_newton_method(change_sign_ranges[range_idx], eps, f, df);
-  double root = method_secants(change_sign_ranges[range_idx], eps, f);
-  std::println("root = {}\n\n", root);
+  std::print("choose method ({} - Bissection, {} - Newton, {} - Newton2, {} - Secants): ",
+    std::to_underlying(SolveMethod::Bissection), std::to_underlying(SolveMethod::Newton),
+    std::to_underlying(SolveMethod::Newton2), std::to_underlying(SolveMethod::Secants));
+  uint32_t solve_method;
+  std::cin >> solve_method;
 
-  return {};
+  double root = solve(change_sign_ranges[range_idx], eps, f, df, static_cast<SolveMethod>(solve_method));
+
+  std::println("root = {}\n\n", root);
 }
 
-static double default_f(double x) { return x * x - 1; }
-static double default_df(double x) { return 2 * x; }
+static double default_f(double x) { return std::pow(x - 2, 2) - 1; }
+static double default_df(double x) { return 2 * (x - 2); }
 
 int main() {
   auto range = Range::parse();
   std::print("input eps: ");
   double eps;
   std::cin >> eps;
-  solve(range, eps, default_f, default_df);
+  interractive_solve(range, eps, default_f, default_df);
 }
---
+++ 3-course/eval-methods/task1/test.cpp
@@ -0,0 +1,61 @@
+#include "solver/solver.hpp"
+
+#include <ranges>
+
+static bool test() {
+  std::array test_funcs {
+    ComputedFunc([](double x) { return x - 10 * std::sin(x); }),
+    ComputedFunc([](double x) { return std::pow(2, -x) - std::sin(x); }),
+    ComputedFunc([](double x) { return std::pow(2, x) - 2 * std::sin(x); }),
+    ComputedFunc([](double x) { return std::sqrt(4 * x + 7) - 3 * std::cos(x); }),
+    ComputedFunc([](double x) { return x * std::sin(x) - 1; }),
+  };
+
+  std::array test_funcs_ders {
+    ComputedFunc([](double x) { return 1 - 10 * std::cos(x); }),
+    ComputedFunc([](double x) { return -std::log(2) * std::pow(2, -x) - std::cos(x); }),
+    ComputedFunc([](double x) { return std::log(2) * std::pow(2, x) - 2 * std::cos(x); }),
+    ComputedFunc([](double x) { return 2 / std::sqrt(4 * x + 7) + 3 * std::sin(x); }),
+    ComputedFunc([](double x) { return std::sin(x) + x * std::cos(x); }),
+  };
+
+  std::array test_ranges {
+    Range(-5, 3),
+    Range(-5, 10),
+    Range(-8, 10),
+    Range(-1.5, 2),
+    Range(-10, 2),
+  };
+
+  std::array test_eps {
+    std::pow(10, -6),
+    std::pow(10, -6),
+    std::pow(10, -6),
+    std::pow(10, -8),
+    std::pow(10, -5),
+  };
+
+  for (uint32_t i = 0; i < SolveMethod::MethodsNum; i++) {
+    auto method = static_cast<SolveMethod>(i);
+    // if (method == SolveMethod::Newton2) continue;
+    for (auto &&[f, df, range, eps] : std::views::zip(test_funcs, test_funcs_ders, test_ranges, test_eps)) {
+      double root = solve(range, eps, f, df, method);
+      if (std::abs(f(root)) > eps) {
+        return false;
+      }
+    }
+    std::println("test of method {} done", i);
+  }
+
+  return true;
+}
+
+int main() {
+  if (test()) {
+    std::println("tests passed");
+    return 0;
+  } else {
+    std::println("tests failed");
+    return -1;
+  }
+}
